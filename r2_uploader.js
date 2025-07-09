077c00b5b643c03ade99ecc1655d99f069040c81
adityavivek11 <adityasharma86410@gmail.com>
2025-07-09 23:58:46 +0530

    cors fixed

--- r2_uploader.js
+++ r2_uploader.js
@@ -13,7 +13,7 @@ const upload = multer({ dest: 'uploads/' });
 
 // Enable CORS for the React app
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173','https://edtechh-dashboard-main-e76s.vercel.app'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173','https://edtechh-dashboard-main-e76s.vercel.app','https://edtechh-dashboard-main-qf79.vercel.app'],
   credentials: true
 }));
 
